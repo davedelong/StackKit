9d2403032afebda6ce451c743ab56aa175d9902e
Dave DeLong <davedelong@me.com>
2010-05-26 17:14:25 -0600

    minor group reorganization and some comments

--- Classes/SKDefinitions.h
+++ Classes/SKDefinitions.h
@@ -8,4 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ There are some cases where constants can have different names but the same value.  For example:
+ - in SKUser:	SKUserID
+ - in SKTag:	SKTagsParticipatedInByUser
+ - in SKBadge:	SKBadgeAwardedToUser
+ 
+ All three of those have the value of @"user_id", but it doesn't always make contextual sense to refer to them all as SKUserID.
+ An SKBadge, for example, does not have an SKUserID.  However, it does know if it has been awarded to a particular user.
+ 
+ To get around the trouble of defining @"user_id" in 3 different places, all three of these constants are declared like so:
+ 
+ NSString * const SKWhateverThisConstantIs = __SKUserID;
+ 
+__SKUserID is #defined below to be @"user_id".
+ 
+ For reference, see http://stackoverflow.com/questions/2909724
+ 
+ **/
+
 #define __SKUserID @"user_id"
