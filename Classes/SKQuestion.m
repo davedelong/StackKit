5944879920ed2edbb2f648376503c2ee33ddf93f
Dave DeLong <davedelong@me.com>
2010-05-29 12:18:44 -0600

    added MIT license information

--- Classes/SKQuestion.m
+++ Classes/SKQuestion.m
@@ -1,12 +1,42 @@
 //
 //  SKQuestion.m
 //  StackKit
-//
-//  Created by Dave DeLong on 1/25/10.
-//  Copyright 2010 Home. All rights reserved.
-//
+/**
+ Copyright (c) 2010 Dave DeLong
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ **/
 
 #import "StackKit_Internal.h"
+
+NSString * const SKQuestionID = @"question_id";
+NSString * const SKQuestionTags = @"tags";
+NSString * const SKQuestionAnswerCount = @"answer_count";
+NSString * const SKQuestionAcceptedAnswer = @"accepted_answer_id";
+NSString * const SKQuestionFavoriteCount = @"favorite_count";
+NSString * const SKQuestionBountyCloseDate = @"bounty_closes_date";
+NSString * const SKQuestionBountyAmount = @"bounty_amount";
+NSString * const SKQuestionCloseDate = @"closed_date";
+NSString * const SKQuestionCloseReason = @"closed_reason";
+NSString * const SKQuestionTimelineURL = @"question_timeline_url";
+NSString * const SKQuestionCommentsURL = @"question_comments_url";
+NSString * const SKQuestionAnswersURL = @"question_answers_url";
 
 @implementation SKQuestion
 
