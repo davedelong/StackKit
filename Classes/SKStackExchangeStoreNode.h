95680f9d546e4a9e38b5429d307791c1b153474c
Jacob Relkin <jacobrelkin@gmail.com>
2011-12-31 08:12:18 -0800

    Created a new NSIncrementalStoreNode subclass, SKStackExchangeStoreNode.

---
+++ Classes/SKStackExchangeStoreNode.h
@@ -0,0 +1,13 @@
+//
+//  SKStackExchangeStoreNode.h
+//  StackKit
+//
+//  Created by Jacob Relkin on 12/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface SKStackExchangeStoreNode : NSIncrementalStoreNode
+
+@end
---
+++ Classes/SKStackExchangeStoreNode.m
@@ -0,0 +1,45 @@
+//
+//  SKStackExchangeStoreNode.m
+//  StackKit
+//
+//  Created by Jacob Relkin on 12/31/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <StackKit/SKStackExchangeStoreNode.h>
+#import <StackKit/StackKit_Internal.h>
+
+@interface SKStackExchangeStoreNode ()
+
+@end
+
+@implementation SKStackExchangeStoreNode
+
+-(id)initWithObjectID:(NSManagedObjectID *)objectID withValues:(NSDictionary *)values version:(uint64_t)version {
+    self = [super initWithObjectID:objectID
+                        withValues:values
+                           version:version];
+    if(self) {
+        //...
+    }
+    return self;
+}
+
+-(void)dealloc {
+    [super dealloc];
+    //...
+}
+
+-(void)updateWithValues:(NSDictionary *)values version:(uint64_t)version {
+    //...
+}
+
+-(id)valueForPropertyDescription:(NSPropertyDescription *)prop {
+    return nil;
+}
+
+-(NSManagedObjectID *)objectID {
+    return nil;
+}
+
+@end
